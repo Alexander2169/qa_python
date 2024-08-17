62f02a24ae9a5af46a1a39625466ce0fc3b63a70
Alexander Cheremisov <dadic2169@mail.ru>
2024-08-17 21:56:28 +0300

    modified_5 test.py

--- tests.py
+++ tests.py
@@ -50,9 +50,45 @@ class TestBooksCollector:
         books = books_collector.get_books_with_specific_genre(genre)
         assert sorted(books) == sorted(name_book)
 
+    def test_get_books_genre_true(self, books_collector):
+        books_collector.add_new_book("Космические пираты")
+        books_collector.add_new_book("Колобок")
 
+        books_collector.set_book_genre("Космические пираты", "Фантастика")
+        books_collector.set_book_genre("Колобок", "Мультфильмы")
 
+        assert books_collector.get_books_genre() == {"Космические пираты":"Фантастика", "Колобок":"Мультфильмы"}
 
+    def tests_get_books_for_children_true(self, books_collector):
+        books_collector.add_new_book("Вий")
+        books_collector.add_new_book("Колобок")
+        books_collector.add_new_book("Приключения Шурика")
+
+        books_collector.set_book_genre("Вий", "Ужасы")
+        books_collector.set_book_genre("Колобок", "Мультфильмы")
+        books_collector.set_book_genre("Приключения Шурика", "Комедии")
+
+        assert sorted(books_collector.get_books_for_children()) == sorted(["Колобок", "Приключения Шурика"])
+
+    def tests_add_book_in_favorites_true(self, books_collector):
+        books_collector.add_new_book("Приключения Шурика")
+        books_collector.add_book_in_favorites("Приключения Шурика")
+        assert "Приключения Шурика" in books_collector.favorites
+
+    def tests_delete_book_from_favorites_true(self, books_collector):
+        books_collector.add_new_book("Приключения Шурика")
+        books_collector.add_book_in_favorites("Приключения Шурика")
+        books_collector.delete_book_from_favorites("Приключения Шурика")
+        assert "Приключения Шурика" not in books_collector.favorites
+
+    def tests_get_list_of_favorites_books_true(self, books_collector):
+        books_collector.add_new_book("Космические пираты")
+        books_collector.add_new_book("Колобок")
+
+        books_collector.add_book_in_favorites("Космические пираты")
+        books_collector.add_book_in_favorites("Колобок")
+
+        assert list(books_collector.get_list_of_favorites_books())
 
 
 
